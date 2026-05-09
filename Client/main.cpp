0fe5915da3b4ee181576a06cddcebe67248f19ab
Roflane <Roflane@roflane.com>
2026-05-09 19:24:09 +0400

    Updated server and client

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,50 +1,64 @@
 #include <iostream>
-#include <string>
 #include <thread>
 #include <asio.hpp>
-#include <random>
 
 using asio::ip::tcp;
 
-int main() {
-    try {
-        asio::io_context io;
+class Client {
+private:
+    asio::io_context _io;
+    tcp::resolver _resolver;
+    tcp::socket _socket;
+    tcp::resolver::results_type _endpoints;
 
-        tcp::resolver resolver(io);
-        const auto endpoints = resolver.resolve("127.0.0.1", "7373");
+public:
+    Client(const std::string& host, const std::string& port)
+        : _resolver(_io),
+          _socket(_io),
+          _endpoints(_resolver.resolve(host, port)) {}
 
-        auto sock = std::make_shared<tcp::socket>(io);
+    asio::io_context& io() { return _io; }
+    tcp::socket& socket() { return _socket; }
 
-        asio::async_connect(*sock, endpoints,
-            [sock](const asio::error_code& ec, const tcp::endpoint&) {
-                if (!ec) {
-                    std::cout << "Connected\n";
-                } else {
-                    std::cerr << "Connect error: " << ec.message() << "\n";
-                }
-            });
-
-        std::thread io_thread([&io]() { io.run(); });
-
-
-        for (;;) {
-            std::string line;
-            if (!std::getline(std::cin, line)) break;
-            line += '\n';
-            if (line.length() > 2) {
-                asio::post(io, [sock, line = std::move(line)]() {
-                     asio::async_write(*sock, asio::buffer(line),
-                         [sock](const asio::error_code& ec, std::size_t bytes) {
-                             if (ec) std::cerr << "Write error: " << ec.message() << "\n";
-                         });
-                 });
-            }
-        }
-
-        io_thread.join();
-    } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
-        return 1;
+    void connect() {
+        asio::connect(_socket, _endpoints);
     }
-    return 0;
+
+    template <typename HandlerT>
+    void run(HandlerT handler) {
+        std::thread io_thread([this] {
+            _io.run();
+        });
+        handler();
+        io_thread.join();
+    }
+};
+
+int main(int argc, char **argv) {
+    try {
+        Client client(argv[1], argv[2]);
+        client.connect();
+        client.run([&client] {
+            std::cout << "Connected\n";
+
+            for (;;) {
+                std::string line;
+                if (!std::getline(std::cin, line))
+                    break;
+
+                line += '\n';
+
+                auto msg = std::make_shared<std::string>(std::move(line));
+                asio::async_write(client.socket(),
+                    asio::buffer(*msg),[msg](const asio::error_code& ec, std::size_t) {
+                        if (ec) {
+                            std::cout << "Write error: " << ec.message() << "\n";
+                        }
+                    });
+            }
+        });
+    }
+    catch (const std::exception& e) {
+        std::cout << e.what() << "\n";
+    }
 }
--- Server/main.cpp
+++ Server/main.cpp
@@ -5,8 +5,11 @@
 
 using asio::ip::tcp;
 
+#define BUFFER_CAP 1024
+
 class Server {
 private:
+   // std::unordered_map<std::string, std::thread> _threadPool;
     std::shared_ptr<asio::io_context> _pIo;
     std::shared_ptr<tcp::acceptor> _pServer;
 public:
@@ -24,17 +27,22 @@ public:
     const auto get_io() const { return _pIo; }
     const auto get_server() const { return _pServer; }
 
+
+    // template <typename T>
+    // void push_thread(T &&thread) {
+    //     _threadPool.emplace(std::forward<T>(thread));
+    // }
+
     template <typename AcceptHandlerT>
     void accept(AcceptHandlerT accept_handler) {
         auto accept_lambda = [this, accept_handler = std::move(accept_handler)](const asio::error_code &ec, tcp::socket socket) {
             accept_handler(ec, std::move(socket));
             this->accept(accept_handler);
         };
-        _pServer->async_accept(*_pIo, accept_lambda);
+        _pServer->async_accept(accept_lambda);
     }
 
 };
-Server server(tcp::v4(), 7373);
 
 void log_accept(const std::error_code &e, const size_t &bytes) {
     printf("Transferred %llu bytes. Status: %s\n", bytes, e.message().c_str());
@@ -44,9 +52,9 @@ void accept_handler(const asio::error_code &ec, tcp::socket socket) {
     try {
         const auto ep = socket.remote_endpoint();
         printf("Accepted connection %s:%d\n", ep.address().to_string().c_str(), ep.port());
-        auto buffer = asio::buffer("", 1024);
+        //auto buffer = asio::buffer("", 1024);
         //asio::async_read(socket, buffer);
-        asio::async_write(socket, asio::buffer("Hello World!\r\n"), std::bind(log_accept, std::placeholders::_1, std::placeholders::_2));
+        asio::async_write(socket, asio::buffer("Hello World!\r\n"), std::bind(std::ref(log_accept), std::placeholders::_1, std::placeholders::_2));
     }
     catch (std::exception &e) {
         std::printf("%s\n", e.what());
@@ -55,55 +63,49 @@ void accept_handler(const asio::error_code &ec, tcp::socket socket) {
 
 
 void accept_client_handler(const asio::error_code &ec, tcp::socket socket) {
-    try {
-        const auto ep = socket.remote_endpoint();
-        printf("Accepted connection %s:%d\n", ep.address().to_string().c_str(), ep.port());
-        auto buf = std::make_shared<asio::streambuf>();
-        auto socket = std::make_shared<tcp::socket>(*server.get_io());
-
-        std::thread([socket = std::move(socket), buf = std::move(buf)]() {
-            std::function<void()> do_read;
-            do_read = [socket, buf, &do_read]() {
-               socket->async_read_some(buf->prepare(1024),
-                   [socket, buf, &do_read](std::error_code ec, std::size_t bytes) {
-                       if (bytes > 0) {
-                           buf->commit(bytes);
-                           std::istream is(buf.get());
-                           std::string data(bytes, 0);
-                           is.read(&data[0], bytes);
-                           std::cout << data << std::endl;
-
-                           printf("Read %d bytes\n", bytes);
-
-                           buf->consume(bytes);
-                           do_read();
-                       } else {
-                           if (ec != asio::error::eof) {
-                               std::cerr << ec.message() << std::endl;
-                           }
-                           std::printf("Disconnected: %s\n", socket->remote_endpoint().address().to_string().c_str());
-                           socket->close();
-                       }
-                   });
-                do_read();
-           };
-           do_read();
-        }).detach();
+    if (ec) {
+        std::cerr << "Accept error: " << ec.message() << "\n";
+        return;
     }
-    catch (std::exception &e) {
-       std::printf("%s\n", e.what());
-    }
-};
 
+    const auto ep = socket.remote_endpoint();
+    std::cout << "Accepted connection "
+              << ep.address().to_string()
+              << ":"
+              << ep.port()
+              << "\n";
 
-int main() {
+    auto pSocket = std::make_shared<tcp::socket>(std::move(socket));
+    auto pBuffer = std::make_shared<std::array<char, BUFFER_CAP>>();
+    auto read_handler = std::make_shared<std::function<void(const asio::error_code&, std::size_t)>>();
+
+    *read_handler = [pSocket, pBuffer, read_handler](const asio::error_code& ec, std::size_t bytes){
+        if (ec) {
+            std::cout << "Disconnected: " << ec.message() << "\n";
+            return;
+        }
+
+        std::cout.write(pBuffer->data(), bytes);
+        std::cout.flush();
+
+        pSocket->async_read_some(
+            asio::buffer(*pBuffer),
+            *read_handler
+        );
+    };
+
+    pSocket->async_read_some(asio::buffer(*pBuffer), *read_handler);
+}
+
+int main(int argc, char **argv) {
     try {
+        Server server(tcp::v4(), std::atoi(argv[1]));
         const auto local_ep = server.get_server()->local_endpoint();
         printf("Started listening on %s:%d\n", local_ep.address().to_string().c_str(), local_ep.port());
         server.accept(std::ref(accept_client_handler));
         server.get_io()->run();
     }
     catch (std::exception &e) {
-        std::printf("%s\n", e.what());
+        std::cerr << e.what() << "\n";
     }
 }
